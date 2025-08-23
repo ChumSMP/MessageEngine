471a28fa6bd1461453d1698078c630fd79f5df44
Myles "Mellurboo" Wilson <mellurblur@gmail.com>
2025-08-23 15:45:09 +0100

    Init Commit Project Creation

---
+++ src/main/java/org/mellurboo/messageEngine/MessageEngine.java
@@ -0,0 +1,17 @@
+package org.mellurboo.messageEngine;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MessageEngine extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
